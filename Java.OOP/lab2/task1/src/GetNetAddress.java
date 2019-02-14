f9779c3e90794164705b118fc3af8088cad05267
fkudriak <amdiag@ya.ru>
2019-02-14 15:50:51 +0300

    init. Lab 2

---
+++ Java.OOP/lab2/task1/src/GetNetAddress.java
@@ -0,0 +1,20 @@
+class GetNetAddress {
+  public static void main(String[] args) {
+    //put your task here
+    //Scanner in = new Scanner(System.in);
+    if ( args.length != 2 ){
+      System.out.println("GetNetAddress <IP address> <Subnet mask>");
+      return;
+    }
+
+    byte[] address = args[0].getBytes();
+    byte[] mask = args[1].getBytes();
+
+    //if (){}
+
+
+    byte[] result = mask;// & address.getBytes();
+    System.out.println(result);
+
+  }
+}
