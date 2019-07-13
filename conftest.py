ca5ec730e1ff40305a066f04530fe6b13e6c2d05
amdiag <amdiag@ya.ru>
2019-07-13 11:34:12 +0300

    It's done exercise module 3.6

--- conftest.py
+++ conftest.py
@@ -1,12 +1,22 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help="Type two symbols language code, e.g. 'ru', 'en', 'es', 'fr'")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     language = request.config.getoption("language")
-    # print("\nstart browser for test..")
-    # browser = webdriver.Chrome()
-    # yield browser
-    # print("\nquit browser..")
-    # browser.quit()
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+
+    browser = webdriver.Chrome(options=options)
+    print("\nstart browser for test..")
+
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,9 +1,13 @@
-import pytest
-from selenium import webdriver
+import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_guest_should_see_button_add_to_basket(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("#login_link")
+    classNameToBasket = "btn-add-to-basket"
+    btnToBasket = browser.find_elements_by_class_name(classNameToBasket)
+    # print( btnToBasket )
+    assert len(
+        btnToBasket) == 1, "Too much buttons or button isn't exist"  # если кнопок с таким селектором больше 1 или они отсутствуют падает
+    # time.sleep(30)
